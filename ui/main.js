402c94390c7dccb4c131aa3373b728f81b50123e
Sharim1 <sarimpervez@gmail.com>
2016-10-04 15:09:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,15 @@ var element = document.getElementById('main-txt'
 element.innerHTML = 'New Value';
 //move image modi
 var img= document.getElementById('madi');
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft +'px';
+    }
 img.onclick= function () {
-    img.style.marginLeft = '100px';
+   var interval=setInterval(moveRight,100);
+  
+
     
     
 };
